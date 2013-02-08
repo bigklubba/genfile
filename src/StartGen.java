d33a2232f5d12501a08430fa4e95ee5a30b0e766
Pierre Emrikstrand <pierre.emrikstrand@gmail.com>
2013-02-08 14:08:33 +0100

    adding the file structure

---
+++ src/StartGen.java
@@ -0,0 +1,15 @@
+public class StartGen {
+
+    public static void main(String args[]) {
+	String pre = "Game.baypatrol.Client|GameName";
+	String mid = "|message=";
+	String after = "text";
+
+	int start = Integer.parseInt(args[0]);
+	int end = Integer.parseInt(args[1]);
+
+	for(int i = start; i < end; i++) {
+	    System.out.println(pre+i+mid+after+i);
+	}
+    }
+}
