63f75aea17b5abdc79bb4f22dcc0c88c5959801c
Pierre Emrikstrand <pierre.emrikstrand@gmail.com>
2013-02-13 08:31:43 +0100

    added new maven structure

---
+++ src/main/java/org/whennaturecalls/bigklubba/genfile/StartGen.java
@@ -0,0 +1,11 @@
+package org.whennaturecalls.bigklubba.genfile;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: emrpie
+ * Date: 2/13/13
+ * Time: 8:28 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StartGen {
+}
