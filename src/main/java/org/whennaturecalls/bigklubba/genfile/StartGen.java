2e65daa5987706dde06187f9db541490f7c8c3ba
Pierre Emrikstrand <pierre.emrikstrand@gmail.com>
2013-02-13 08:32:45 +0100

    updated the genfile class to use the package

--- src/main/java/org/whennaturecalls/bigklubba/genfile/StartGen.java
+++ src/main/java/org/whennaturecalls/bigklubba/genfile/StartGen.java
@@ -7,5 +7,20 @@ package org.whennaturecalls.bigklubba.genfile;
  * Time: 8:28 AM
  * To change this template use File | Settings | File Templates.
  */
+/**
+ * Just a small program to generate text
+ */
 public class StartGen {
+    public static void main(String args[]) {
+        String pre = "Game.baypatrol.Client|GameName";
+        String mid = "|message=";
+        String after = "text";
+
+        int start = Integer.parseInt(args[0]);
+        int end = Integer.parseInt(args[1]);
+
+        for(int i = start; i < end; i++) {
+            System.out.println(pre+i+mid+after+i);
+        }
+    }
 }
